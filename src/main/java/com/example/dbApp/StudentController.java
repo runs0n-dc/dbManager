b933bafacb1dac738e553ceed70e80cf4f90964c
Debojeet Chakraborty <17debojeet@gmail.com>
2022-12-11 20:04:00 +0530

    Second

--- src/main/java/com/example/dbApp/StudentController.java
+++ src/main/java/com/example/dbApp/StudentController.java
@@ -1,5 +1,8 @@
 package com.example.dbApp;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,16 +13,20 @@ import java.sql.SQLException;
 import java.util.List;
 
 @RestController
+//@Slf4j
 public class StudentController {
     @Autowired
     dbManager db;
+    Logger logger=LoggerFactory.getLogger(StudentController.class);
 
 @PostMapping("/insert_info")
     public void insertDetails(@RequestBody Student student) throws SQLException {
     db.insertInfo(student);
 }
 @GetMapping("/all_info")
+
     public List<Student> getAllInfo() throws SQLException {
+    logger.info("Kaam_CHALU_HAI");
     return db.getALL();
 }
 }
